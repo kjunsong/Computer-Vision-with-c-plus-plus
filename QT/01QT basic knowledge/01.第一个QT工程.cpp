b9751336ebe35ef5cdb2fc3b09f14befb7337fee
kjunsong <35649658+kjunsong@users.noreply.github.com>
2018-01-27 15:06:53 +0800

    Update 01.第一个QT工程.cpp

--- QT/01QT basic knowledge/01.第一个QT工程.cpp
+++ QT/01QT basic knowledge/01.第一个QT工程.cpp
@@ -1,1 +1,16 @@
+#include "mywidget.h"
+#include <QApplication> //QT头文件没有.h QApplication应用程序类，文件名和类名一样
 
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);//有且只有一个应用程序类对象
+
+    //w就是一个窗口类
+    myWidget w; //MyWidget是一个窗口类，它继承于QWidget（QWidget是一个窗口基类）
+
+    w.show(); //窗口创建默认是隐藏的，需要设置显示
+
+    a.exec(); //让程序一直执行，等待事件发生（用户操作）
+    return 0;
+    //return a.exec();
+}
